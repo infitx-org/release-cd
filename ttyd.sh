169c478dd9f1986d55ebd0b9be2cae20bf550d82
Kalin Krustev <kalin.krustev@gmail.com>
2025-11-18 08:58:41 +0000

    fix: add check for .bashrc and copy skeleton files if missing

--- ttyd.sh
+++ ttyd.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
+# if .bashrc is missing from the home directory when volume is mounted, copy all files from /etc/skel for the current user
+USER=$(whoami)
+if [ ! -f /home/"${USER}"/.bashrc ]; then
+    cp -r /etc/skel/. /home/"${USER}"/
+    chown -R "${USER}":"${USER}" /home/"${USER}"/
+fi
+
 ttyd -a -O -W -m 5 -p 8080 -c ${DEV_USER}:${DEV_PASS} bash ./tui.sh
