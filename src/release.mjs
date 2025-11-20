ed74369dc646cb9f0b75f5e7d9dd6332fad78b93
Kalin Krustev <kalin.krustev@gmail.com>
2025-11-20 22:32:39 +0000

    fix: pass content type

--- src/release.mjs
+++ src/release.mjs
@@ -7,9 +7,12 @@ export default async function notifyRelease({
     const { url } = config.release;
     if (!url || !reportId) return;
     console.log(`Notifying release at ${url} for report ${reportId}`);
-    await fetch(url, {
-        method: 'POST', body: JSON.stringify({
-            [`tests.${reportId}`]: summary
-        })
-    });
+    await fetch(
+        url,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ [`tests.${reportId}`]: summary })
+        }
+    );
 }
