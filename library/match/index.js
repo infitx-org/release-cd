2c5a5c05ef34ddb013a610ba4357f34185e60eed
Kalin Krustev <kalin.krustev@gmail.com>
2025-12-23 17:33:26 +0000

    fix: improve match function for nullish values

--- library/match/index.js
+++ library/match/index.js
@@ -2,14 +2,16 @@ const isMatchWith = require('lodash/isMatchWith');
 const isPlainObject = require('lodash/isPlainObject');
 
 function match(value, condition) {
-    if (value == null || condition == null) {
-        return false;
+    if (value == null && condition == null) {
+        return true
     } else if (value === condition) {
         return true;
     } else if (Array.isArray(condition) || Array.isArray(value)) {
         return Array.isArray(value)
             ? value.some(v => module.exports(v, condition))
             : condition.some(v => module.exports(value, v));
+    } else if (value == null || condition == null) {
+        return false;
     } else if (condition instanceof RegExp) {
         if (typeof value === 'string') return condition.test(value);
     } else if (condition instanceof Date) {
@@ -28,7 +30,7 @@ function match(value, condition) {
         case 'function':
             return condition(value);
         case 'object': {
-            let {min, max} = condition;
+            let { min, max } = condition;
             if (value instanceof Date) {
                 value = value.getTime();
                 if (!Number.isFinite(value)) return false;
@@ -47,11 +49,13 @@ function match(value, condition) {
             if (max != null && (value > max || value === Infinity || max === -Infinity))
                 return false;
             if (min != null || max != null) return true;
+            if (typeof value === 'object' && value && condition) return module.exports(value, condition);
         }
     }
 }
 
 module.exports = function (factValue, ruleValue) {
+    if (factValue === ruleValue) return true;
     if (
         Array.isArray(factValue) ||
         Array.isArray(ruleValue) ||
@@ -59,6 +63,9 @@ module.exports = function (factValue, ruleValue) {
         !isPlainObject(ruleValue)
     )
         return match(factValue, ruleValue);
-
+    if (factValue && ruleValue && typeof factValue === 'object' && typeof ruleValue === 'object') {
+        const nullFilter = Object.entries(ruleValue).filter(([, value]) => value == null || Array.isArray(value));
+        if (nullFilter.length > 0) factValue = { ...Object.fromEntries(nullFilter), ...factValue };
+    };
     return isMatchWith(factValue, ruleValue, match);
 };
